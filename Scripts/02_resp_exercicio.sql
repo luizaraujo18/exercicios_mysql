71332b47c41187e2f05bf1760f62e2e8402dcc4c
luizaraujo18 <luizaraujo_18@outlook.com>
2025-03-31 18:18:22 -0300

    modulo_01

--- Scripts/02_resp_exercicio.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tbl_produtos (
-	cod_produto int,
-	desc_produto varchar(30),
-	preco_produto float
-);
---
+++ Scripts/03_resp_exercicio.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tbl_nota_fiscal (
+	numero_NF INT,
+  	data_NF DATE,
+  	valor_NF FLOAT
+);
