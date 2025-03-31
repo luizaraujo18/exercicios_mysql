8352d0ba19a498aa6e6e404f044579276dcb46e2
luizaraujo18 <luizaraujo_18@outlook.com>
2025-03-31 18:37:25 -0300

    modulo_01-rev1

---
+++ Scripts/06_resp_exercicio.sql
@@ -0,0 +1,4 @@
+ALTER TABLE tbl_nota_fiscal
+ADD CONSTRAINT PK_Nota_Fiscal 
+PRIMARY KEY (numero_NF);
+
---
+++ Scripts/07_resp_exercicio.sql
@@ -0,0 +1,4 @@
+ALTER TABLE tbl_vendas
+ADD CONSTRAINT PK_Itens 
+PRIMARY KEY(produto_cod_produto, nota_fiscal_numero_NF);
+
